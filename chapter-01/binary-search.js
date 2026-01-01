5a1b2e9f6c1ff9917e29adf121e48ce9bd715eeb
Avion C. <83898002+AvionShea@users.noreply.github.com>
2026-01-01 18:10:19 -0500

    Add C++ binary search implementation

--- chapter-01/binary-search.js
+++ chapter-01/binary-search.js
@@ -24,3 +24,37 @@ const myArrList = [1, 3, 5, 7, 9];
 console.log(binarySearch(myArrList, -1), null);
 console.log(binarySearch(myArrList, 5), 2);
 console.log(binarySearch(myArrList, 1), 0);
+
+//C++ Solution
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int binarySearch(vector <int> arr, int number){
+int low = 0;
+int high = arr.size() - 1;
+
+    while (low <= high) {
+        int mid = (low + high) / 2;
+        int guess = arr[mid];
+
+        if (guess == number) {
+            return mid;
+        } else if (guess > number) {
+            high = mid - 1;
+        } else {
+            low = mid + 1;
+        }
+    }
+    return -1;
+}
+
+int main(){
+    vector <int> myArrList = { 1, 3, 5, 7, 9 };
+
+    cout << binarySearch(myArrList, -1) << " (expected: -1)" << endl;
+    cout << binarySearch(myArrList, 5) << " (expected: 2)" << endl;
+    cout << binarySearch(myArrList, 1) << " (expected: 0)" << endl;
+
+    return 0;
+}
